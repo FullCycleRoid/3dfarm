d5725096ba7f953acdc0427878b07871ac022241
ruby roid <andrewthechf@gmail.com>
2021-09-01 13:33:15 +0300

    123

--- server/index.js
+++ server/index.js
@@ -1,49 +1,16 @@
-const cors = require("cors");
 const path = require('path');
-const fileupload = require("express-fileupload");
-const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const express = require("express");
 const PORT = process.env.PORT || 3100;
 const app = express();
 
-const corsOptions ={
-   origin:'*',
-   credentials:true,
-   methods: "GET, POST",
-   optionSuccessStatus:200,
-}
 
-let allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Headers', "*");
-  next();
-}
-app.use(allowCrossDomain);
-
-app.use('/upload', createProxyMiddleware({ target: 'http://localhost:3100', changeOrigin: true }));
-app.use(fileupload());
 app.use(express.static(path.resolve(__dirname, '../client/build')));
-
 
 
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
-});
-
-
-app.post("/upload", (req, res) => {
-  const newpath = __dirname + "/files/";
-  const file = req.files.file;
-  const filename = file.name;
-  console.log("file upload try")
-  file.mv(`${newpath}${filename}`, (err) => {
-    if (err) {
-      res.status(500).send({ message: "File upload failed", code: 200 });
-    }
-    res.status(200).send({ message: "File Uploaded", code: 200 });
-  });
 });
 
 
